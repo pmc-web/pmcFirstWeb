98f70cc0dbc4bf31139adec16edf3169f11a69ed
kymkyj <ymyj33@naver.com>
2020-10-17 04:47:47 +0900

    Home 주석제거

--- src/main/java/com/bootproj/pmcweb/Controller/HomeController.java
+++ src/main/java/com/bootproj/pmcweb/Controller/HomeController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-//    @GetMapping("/")
-//    public String home(){
-//
-//        return "main";
-//    }
+    @GetMapping("/")
+    public String home(){
+
+        return "main";
+    }
 }
