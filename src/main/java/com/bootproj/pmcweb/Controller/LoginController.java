57668756ec5657a6844922c466e1cece49297aa5
kymkyj <ymyj33@naver.com>
2020-10-24 11:33:53 +0900

    git commit

--- src/main/java/com/bootproj/pmcweb/Controller/LoginController.java
+++ src/main/java/com/bootproj/pmcweb/Controller/LoginController.java
@@ -4,8 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.Optional;
-
 @Controller
 public class LoginController {
 
