baa840744cbccfceabf71c32eba63c5498525a84
jiae <hirlawldo413@gmail.com>
2020-10-31 09:55:46 +0900

    [feat] 헤더, Result Code 추가

---
+++ src/main/java/com/bootproj/pmcweb/Network/Header.java
@@ -0,0 +1,54 @@
+package com.bootproj.pmcweb.Network;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Header<T> {
+    // api 통신 시간
+    private LocalDateTime transactionTime;
+
+    // api 응답 코드
+    private String resultCode;
+
+    // api 부가 설명
+    private String description;
+
+    private T data;
+
+    // OK , No Data (200)
+    public static <T> Header<T> OK() {
+        return (Header.<T>builder()
+                .transactionTime(LocalDateTime.now())
+                .resultCode(ResultCode.RESULT_OK)
+                .description("OK")
+                .build());
+    }
+
+    // OK , Data (200)
+    public static <T> Header<T> OK(T data) {
+        return (Header.<T>builder()
+                .transactionTime(LocalDateTime.now())
+                .resultCode(ResultCode.RESULT_OK)
+                .description("OK")
+                .data(data)
+                .build());
+    }
+
+    // ERROR
+    public static <T> Header<T> Error(String description) {
+        return (Header.<T>builder()
+                .transactionTime(LocalDateTime.now())
+                .resultCode(ResultCode.RESULT_ERROR)
+                .description(description)
+                .build());
+    }
+}
---
+++ src/main/java/com/bootproj/pmcweb/Network/ResultCode.java
@@ -0,0 +1,6 @@
+package com.bootproj.pmcweb.Network;
+
+public class ResultCode {
+    public static final String RESULT_OK = "OK";
+    public static final String RESULT_ERROR = "ERROR";
+}
