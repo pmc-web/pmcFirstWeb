1fa8e498025db99232dc54f1ffea3f373230ddd5
SONG22 <ansongi527@gmail.com>
2020-11-07 18:54:26 +0900

    [fix][Study] 스터디 status, type enum으로 변경

--- src/main/java/com/bootproj/pmcweb/Domain/Study.java
+++ src/main/java/com/bootproj/pmcweb/Domain/Study.java
@@ -1,9 +1,6 @@
 package com.bootproj.pmcweb.Domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
@@ -19,17 +16,14 @@ public class Study{
     private Date instTime;
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date updtTime;
-
-    private Integer status; // String -> Integer default:0 (스터디진행중)
+    private String status;
     private String description;
-
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date startDate;
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date endDate;
-
     private Integer evaluation;
-    private Integer type; // String -> Integer (무료-0, 유료-1)
+    private String type;
     private Long subjectId;
     private Long regionId;
 }
