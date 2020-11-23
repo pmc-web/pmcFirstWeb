9f2e0a7349ded66944c612bd6b3f217cc5034e09
kymkyj <ymyj33@naver.com>
2020-11-24 01:29:03 +0900

    [fix] 시큐리티 메일인증 실패시 로그인 안되게 변경

---
+++ src/main/java/com/bootproj/pmcweb/Network/Exception/NoAuthEmailException.java
@@ -0,0 +1,11 @@
+package com.bootproj.pmcweb.Network.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code= HttpStatus.NOT_ACCEPTABLE, reason="이메일 인증 되지 않음")
+public class NoAuthEmailException extends Exception {
+    public NoAuthEmailException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/bootproj/pmcweb/Network/Request/LoginFailHandler.java
@@ -0,0 +1,41 @@
+package com.bootproj.pmcweb.Network.Request;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.XSlf4j;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.CredentialsExpiredException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Getter
+@Setter
+@Slf4j
+public class LoginFailHandler implements AuthenticationFailureHandler {
+    private final String DEFAULT_FAILURE_URL = "/user/loginFail";
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        String errorMsg = null;
+
+        if(exception instanceof BadCredentialsException){
+            errorMsg = "아이디나 비밀번호가 맞지 않습니다. 다시 확인해 주세요.";
+        }else if(exception instanceof InternalAuthenticationServiceException){
+            errorMsg = "이메일이 인증되지 않았습니다. 이메일을 확인해 주세요.";
+        }else if(exception instanceof DisabledException){
+            errorMsg = "계정이 비활성화 되었습니다. 관리자에게 문의하세요.";
+        }else{
+            errorMsg = "알수없는 이유로 로그인에 실패하였습니다.";
+        }
+        request.setAttribute("errorMsg", errorMsg);
+        request.getRequestDispatcher(DEFAULT_FAILURE_URL).forward(request, response);
+    }
+}
