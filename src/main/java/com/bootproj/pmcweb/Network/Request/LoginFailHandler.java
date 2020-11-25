592cfa81774c1439cbfdc5562b987f543cbe4e6e
kymkyj <ymyj33@naver.com>
2020-11-26 01:16:17 +0900

    [fix] security Exception error success

--- src/main/java/com/bootproj/pmcweb/Network/Request/LoginFailHandler.java
+++ src/main/java/com/bootproj/pmcweb/Network/Request/LoginFailHandler.java
@@ -4,13 +4,12 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import lombok.extern.slf4j.XSlf4j;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.CredentialsExpiredException;
-import org.springframework.security.authentication.DisabledException;
-import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.authentication.*;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
+import javax.security.auth.login.AccountException;
+import javax.security.auth.login.AccountExpiredException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -28,10 +27,11 @@ public class LoginFailHandler implements AuthenticationFailureHandler {
 
         if(exception instanceof BadCredentialsException){
             errorMsg = "아이디나 비밀번호가 맞지 않습니다. 다시 확인해 주세요.";
-        }else if(exception instanceof InternalAuthenticationServiceException){
-            errorMsg = "이메일이 인증되지 않았습니다. 이메일을 확인해 주세요.";
-        }else if(exception instanceof DisabledException){
+        }else if(exception instanceof DisabledException) {
             errorMsg = "계정이 비활성화 되었습니다. 관리자에게 문의하세요.";
+        }else if(exception instanceof LockedException){
+            log.info("이메일이 인증되지 않았습니다. 이메일을 확인해 주세요.");
+            errorMsg = "이메일이 인증되지 않았습니다. 이메일을 확인해 주세요.";
         }else{
             errorMsg = "알수없는 이유로 로그인에 실패하였습니다.";
         }
