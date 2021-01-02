a5c162b243deaf4645f605fa22bb46d1651b4f1a
kymkyj <ymyj33@naver.com>
2021-01-02 11:16:03 +0900

    [fix] 지도 부분 변경

--- src/main/resources/static/js/kakaodata.js
+++ src/main/resources/static/js/kakaodata.js
@@ -1,15 +1,15 @@
 var data = [
     {
-        title: "서울",
-        content: "서울 남산",
+        title: "스프링 스터디",
+        description: "스프링 스터디 입니다.",
         date: "2020-07-10",
         lat: 37.550975,
         lng: 126.991107
 
     },{
-        title: "서울",
-        content: "서울 용산",
-        date: "2020-07-11",
+        title: "자바 스터디",
+        description: "자바 스터디 입니다.",
+        date: "2020-09-11",
         lat: 37.531337,
         lng: 126.979729
     },
