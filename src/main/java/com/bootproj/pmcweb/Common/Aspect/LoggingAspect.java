54f76d213fe56309311459fa25c0cbcfb4aa8743
jiae <hirlawldo413@gmail.com>
2021-01-04 18:34:22 +0900

    [common][fix] logging aspect 버그 수정
    - websocket 메세지일 경우 attribute가 없기 때문에 IlligalException이 터질 수 있음.

--- src/main/java/com/bootproj/pmcweb/Common/Aspect/LoggingAspect.java
+++ src/main/java/com/bootproj/pmcweb/Common/Aspect/LoggingAspect.java
@@ -30,9 +30,11 @@ public class LoggingAspect {
         try {
             return proceedingJoinPoint.proceed(proceedingJoinPoint.getArgs());
         } finally {
-//            HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
-//            long end = System.currentTimeMillis();
-//            log.info("Request: {} {} : {} ({}ms)", request.getMethod(), request.getRequestURL(), paramMapToString(request.getParameterMap()),end - start);
+            if (RequestContextHolder.getRequestAttributes() != null) {
+                HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+                long end = System.currentTimeMillis();
+                log.info("Request: {} {} : {} ({}ms)", request.getMethod(), request.getRequestURL(), paramMapToString(request.getParameterMap()),end - start);
+            }
         }
     }
 
