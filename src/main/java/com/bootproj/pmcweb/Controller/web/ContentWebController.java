5c6978db3460738b7b279796118e35339187b0a0
kymkyj <ymyj33@naver.com>
2021-01-08 23:13:20 +0900

    [fix] 지도 부분 JSON Data로 변경

--- src/main/java/com/bootproj/pmcweb/Controller/web/ContentWebController.java
+++ src/main/java/com/bootproj/pmcweb/Controller/web/ContentWebController.java
@@ -3,6 +3,7 @@ package com.bootproj.pmcweb.Controller.web;
 import com.bootproj.pmcweb.Service.StudyService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONArray;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
@@ -28,9 +29,9 @@ public class ContentWebController {
         Date currentTime = new Date ();
         String sysdate = mSimpleDateFormat.format ( currentTime );
         List<String> studyAllList = studyService.getAllList();
-        log.info("testdata :"+studyAllList);
+        JSONArray jsonArray = new JSONArray(studyAllList);
 
-        model.addAttribute("studyAllList", studyAllList);
+        model.addAttribute("studyAllList", jsonArray);
         model.addAttribute("content", 1);
         model.addAttribute("getdate", sysdate);
 
