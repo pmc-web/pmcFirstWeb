91cdbf412d1417e76c04776344c193d79b8e3de8
jiae <37948906+jiaekim123@users.noreply.github.com>
2021-01-15 23:31:19 +0900

    [attachment][fix] resource handler 추가
    - /out/img -> 외부 이미지 경로 매핑

--- src/main/java/com/bootproj/pmcweb/Config/WebMvcConfig.java
+++ src/main/java/com/bootproj/pmcweb/Config/WebMvcConfig.java
@@ -2,16 +2,22 @@ package com.bootproj.pmcweb.Config;
 
 import com.bootproj.pmcweb.Common.Intercepter.LoggingInterceptor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @RequiredArgsConstructor
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-
+    final private Environment env;
     final LoggingInterceptor loggingInterceptor;
+    private final String PROFILE_PATH_PROPERTY = "profile.image.path";
+    private final String LOCAL_WINDOW = "C:";
+    private final String LINUX_MAC = "/";
+    private final String OUT_IMAGE =  "/out/img/";
+
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
@@ -19,5 +25,17 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addInterceptor(loggingInterceptor)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/vendor/**", "/css/*", "/img/*", "/alarm/*");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        if (env.getProperty(PROFILE_PATH_PROPERTY).startsWith(LOCAL_WINDOW)){
+            registry.addResourceHandler(OUT_IMAGE + "**")
+                    .addResourceLocations("file:///");
+        } else if (env.getProperty(PROFILE_PATH_PROPERTY).startsWith(LINUX_MAC)){
+            registry.addResourceHandler(OUT_IMAGE + "**")
+                    .addResourceLocations("file:/");
+        }
+
     }
 }
